89ffd6236f743881e33e004c4555ea79472f59ee
jaymeswhyte <jmwhyte2002@gmail.com>
2023-10-12 21:51:43 +0100

    Tkinter GUI created

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-import tkinter
+import tkinter as tk
+
+def buttonPress():
+    print("Button Pressed")
+
+root = tk.Tk()
+root.title("PrioriTask")
+root.geometry("1280x720")
+
+button = tk.Button(root, text="Test", command=buttonPress)
+button.pack()
+
+addTaskView = tk.Frame(root)
+
+root.mainloop()
