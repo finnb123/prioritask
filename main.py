5b3899a2b38d72954ab84fb6114e68b1d1710a81
jaymeswhyte <jmwhyte2002@gmail.com>
2023-10-12 22:12:37 +0100

    Added "add task" view

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import tkinter as tk
 
-def buttonPress():
-    print("Button Pressed")
+def showAddTaskView():
+    print("showing addtaskview")
+    addTaskView.pack(fill="both", expand=True)
+
+def hideAddTaskView():
+    print("hiding addtaskview")
+    addTaskView.pack_forget()
+
+def plusPress():
+    print("+ Button Pressed")
+    showAddTaskView()
 
 root = tk.Tk()
 root.title("PrioriTask")
 root.geometry("1280x720")
 
-button = tk.Button(root, text="Test", command=buttonPress)
-button.pack()
+plusButton = tk.Button(root, text="+", command=plusPress)
+plusButton.pack()
 
-addTaskView = tk.Frame(root)
+addTaskView = tk.Frame(root, bg="black")
+testLabel = tk.Label(addTaskView, text="Test")
+testLabel.pack()
+taskViewExit = tk.Button(addTaskView, text="exit", command=hideAddTaskView)
+taskViewExit.pack()
+
+
 
 root.mainloop()
